2f47bb189867049a6e0e5d9109439dced20899ae
AnthonyDawson <31189934+AnthonyDawson@users.noreply.github.com>
2019-03-18 22:18:52 +0700

    Add files via upload

---
+++ admin/partials/rss_poster-admin-display.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * Provide a admin area view for the plugin
+ *
+ * This file is used to markup the admin-facing aspects of the plugin.
+ *
+ * @link       https://www.anthonydawson.com
+ * @since      1.0.0
+ *
+ * @package    Rss_poster
+ * @subpackage Rss_poster/admin/partials
+ */
+?>
+
+<!-- This file should primarily consist of HTML with a little bit of PHP. -->
